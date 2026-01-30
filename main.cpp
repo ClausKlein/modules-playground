3f427864a580e5ba5dc1435b84fdc3fa82c8ba4b
Ruben Perez <rubenperez038@gmail.com>
2026-01-30 17:52:48 +0100

    import std tests

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
-import mylib;
+import std;
+#include <cassert>      // assert
+#include <cerrno>       // errno, all E* errno macros
+#include <cstdarg>      // va_arg, va_copy, va_end, va_start
+#include <version>      // __cplusplus, __cpp_lib_*, __cpp_*, etc.
+#include <climits>      // integer limit macros (same as C)
+#include <cfloat>       // floating-point limit/characteristic macros (same as C)
+#include <cstdint>      // fixed-width limit and optional format macros (same as C)
+#include <cinttypes>    // format/scan macros (same as C)
+
+// #include <cstddef>      // NULL, offsetof
+// #include <cmath>        // HUGE_VAL*, INFINITY, NAN, FP_*, MATH_*, math_errhandling
+// #include <csignal>      // SIG_* macros (same as C)
+// #include <csetjmp>      // setjmp (and any other C macros from <setjmp.h>)
+// #include <ctime>        // time/clocks macros (same as C)
+// #include <clocale>      // LC_* and other C locale macros
 
 int main()
 {
-    mylib::conv("hola");
 }
