e5406f3e91878d3ce80193c73d6e6a199a6ac2af
ClausKlein <claus.klein@arcormail.de>
2026-03-23 16:36:22 +0100

    The final cut

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,20 +1,38 @@
+#ifdef PLAY_HAS_IMPORT_STD
 import std;
-#include <cassert>      // assert
-#include <cerrno>       // errno, all E* errno macros
-#include <cstdarg>      // va_arg, va_copy, va_end, va_start
-#include <version>      // __cplusplus, __cpp_lib_*, __cpp_*, etc.
-#include <climits>      // integer limit macros (same as C)
-#include <cfloat>       // floating-point limit/characteristic macros (same as C)
-#include <cstdint>      // fixed-width limit and optional format macros (same as C)
-#include <cinttypes>    // format/scan macros (same as C)
+#else
+# include <iostream>
+#endif
 
-// #include <cstddef>      // NULL, offsetof
-// #include <cmath>        // HUGE_VAL*, INFINITY, NAN, FP_*, MATH_*, math_errhandling
-// #include <csignal>      // SIG_* macros (same as C)
-// #include <csetjmp>      // setjmp (and any other C macros from <setjmp.h>)
-// #include <ctime>        // time/clocks macros (same as C)
-// #include <clocale>      // LC_* and other C locale macros
+#include <cassert>    // assert
+#include <cerrno>     // errno, all E* errno macros
+#include <cfloat>     // floating-point limit/characteristic macros (same as C)
+#include <cinttypes>  // format/scan macros (same as C)
+#include <climits>    // integer limit macros (same as C)
+#include <cstdarg>    // va_arg, va_copy, va_end, va_start
+#include <cstdint>    // fixed-width limit and optional format macros (same as C)
+#include <cstdlib>    // EXIT_SUCCESS, EXIT_FAILURE
+#include <version>    // __cplusplus, __cpp_lib_*, __cpp_*, etc.
 
-int main()
-{
+// /usr/local/Cellar/llvm/22.1.1/bin/../include/c++/v1/__type_traits/promote.h:38:7:
+// error: type alias template redefinition with different types
+// FIXME: #include <cmath>        // HUGE_VAL*, INFINITY, NAN, FP_*, MATH_*, math_errhandling
+
+#include <clocale>  // LC_* and other C locale macros
+#include <csetjmp>  // setjmp (and any other C macros from <setjmp.h>)
+#include <csignal>  // SIG_* macros (same as C)
+#include <cstddef>  // NULL, offsetof
+#include <ctime>    // time/clocks macros (same as C)
+
+int main() {
+    // use current time as seed for random generator
+    std::srand(std::time(NULL));  // NOLINT
+
+    std::cout << "RAND_MAX: " << RAND_MAX << '\n'
+              << "INT_MAX: " << INT_MAX << '\n'
+              << "INFINITY: " << INFINITY << '\n'
+              << "Random value on [0,1]: "
+              // NOLINTNEXTLINE
+              << static_cast<double>(std::rand()) / RAND_MAX << '\n';
+    return EXIT_SUCCESS;
 }
